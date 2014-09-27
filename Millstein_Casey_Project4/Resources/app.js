f8967ee7c5575cd1eb6a6dc881c86bc022de05d8
cmillstein <cmillstein@fullsail.edu>
2014-09-27 13:16:06 -0400

    CHECK

---
+++ Millstein_Casey_Project4/Resources/app.js
@@ -0,0 +1,46 @@
+
+
+var mainWindow = Ti.UI.createWindow({
+	backgroundColor: "#EEE",
+	fullscreen: true,
+	layout: "vertical"
+});
+
+var header = Ti.UI.createView({
+	height: 30,
+	backgroundColor: "#000",
+	top: 0
+});
+
+var headerLabel = Ti.UI.createLabel({
+	text: "Reddit Data",
+	font: {fontFamily: "Verdana", fontSize: "bold", fontWeight: "bold"},
+	color: "fff"
+});
+
+var scrollView = Ti.UI.createScrollView({
+	height: Ti.Platform.displayCaps.platformHeight - header,
+	layout: "vertical"
+});
+
+
+
+header.add(headerLabel);
+mainWindow.add(header);
+mainWindow.add(scrollView);
+
+
+
+
+var data = require("data");
+var product = require("product");
+var api = require("api");
+
+
+
+
+
+
+
+
+mainWindow.open();
